11dfb4cbec8452ca9029f103ad2c27e4a3282c6f
Zane Stockton <zane.stockton@hotmail.com>
2021-03-08 18:58:38 -0700

    Made Monobehaviour that randomly assigns values to the shader material.
    WIP

---
+++ Assets/AdultLink/VerticalDissolve/Examples/Scripts/RandoMaterial.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandoMaterial : MonoBehaviour 
+{
+	[SerializeField] Material vDissolveMaterial;
+
+	// Use this for initialization
+	public void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () 
+	{
+		
+	}
+
+	private void AssignRandomValues()
+	{
+		Color randomFillColor = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+		vDissolveMaterial.SetColor("_Fillcolor", randomFillColor);
+
+		Color randomRimColor = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+		vDissolveMaterial.SetColor("_Rimlightcolor", randomRimColor);
+
+		vDissolveMaterial.SetFloat("_Rimlightpower", Random.Range(0f, 1f));
+
+		vDissolveMaterial.SetFloat("_Rimlightscale", Random.Range(0f, 1f));
+		
+		vDissolveMaterial.SetFloat("_Rimlightbias", Random.Range(0f, 1f));
+
+		Color randomBaseColor = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+		vDissolveMaterial.SetColor("_Basecolor", randomBaseColor);
+		
+		vDissolveMaterial.SetFloat("_Fill", Random.Range(0f, 1f));
+
+		vDissolveMaterial.SetFloat("_Cutoff", Random.Range(0f, 1f));
+	}
+}
